afb80aad0dca80e242729bbeabba5c39ac545c3c
jinsuSang <twtz0309@naver.com>
2020-08-28 00:26:59 +0900

    주석 추가

--- javascript/lemonade/src/index.js
+++ javascript/lemonade/src/index.js
@@ -1,9 +1,8 @@
 'use strict'
 const express = require('express')
 const path = require('path')
-const fs = require('fs')
-const csv = require('csv-parser')
 const tf = require('@tensorflow/tfjs')
+const readCSV = require('./utils/readCSV')
 
 
 const port = process.env.PORT
@@ -12,18 +11,11 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const app = express()
 app.use(express.static(publicDirectoryPath))
 
-const readCSVtoArr = (csvFilePath) => {
-  return new Promise((resolve, reject) => {
-    const dataSet = []
-    const readStream = fs.createReadStream(csvFilePath)
-    readStream.pipe(csv()).on('error', () => { return reject(new Error('Error reading file')) }).on('data', (data) => { dataSet.push(data) }).on('end', () => { resolve(dataSet) })
-  })
-}
-
 // tensorflow
 const run = async () => {
-  const lemonadeCSV = await readCSVtoArr('./data/lemonade.csv')
+  const lemonadeCSV = await readCSV('./data/lemonade.csv')
 
+  // 데이터를 tensor 형태로 만들기
   const temperature = []
   const sales = []
   for (let i = 0; i < lemonadeCSV.length; i++) {
@@ -37,6 +29,7 @@ const run = async () => {
   independent.print()
   dependent.print()
 
+  // 모델 만들기
   const input = tf.input({ shape: [1] })
   const output = tf.layers.dense({ units: 1 }).apply(input)
   const model = tf.model({
@@ -47,8 +40,13 @@ const run = async () => {
   model.compile({ optimizer: tf.train.adam(), loss: tf.losses.meanSquaredError })
   model.summary()
 
+  // 모델 학습
   const { history: { loss: loss } } = await model.fit(independent, dependent, { epochs: 15000 })
+
+  // 마지막 loss 출력
   console.log(loss[loss.length - 1])
+
+  // 판매량 예측
   model.predict(tf.tensor1d([40])).print()
 }
 
